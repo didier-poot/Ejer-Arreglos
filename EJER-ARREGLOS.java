3673e55a924644dca02047e5c7a46327f078f7c4
Didier <164051232+didier-poot@users.noreply.github.com>
2025-09-21 19:12:17 -0600

    Update EJER-ARREGLOS.java

--- EJER-ARREGLOS.java
+++ EJER-ARREGLOS.java
@@ -1,61 +1,58 @@
-import java.util.*;
+import java.util.Arrays;
 
-class ventas{
+class VentasApp {
     static String[] departamentos = {"ropa", "deportes", "jugueteria"};
-    static String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", 
+    static String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio",
                              "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+    
     static int[][] ventas = new int[12][3];
 
-    public static void insertarVenta(String mes, String departamento, int cantidad) {
+    public static boolean validarDatos(String mes, String departamento) {
         mes = mes.toLowerCase().replace(" ", "");
         departamento = departamento.toLowerCase().replace(" ", "");
-        int indiceMes = Arrays.asList(meses).indexOf(mes);
-        int indiceDepto = Arrays.asList(departamentos).indexOf(departamento);
+        boolean mesValido = Arrays.asList(meses).contains(mes);
+        boolean deptoValido = Arrays.asList(departamentos).contains(departamento);
+        return mesValido && deptoValido;
+    }
 
-        if (indiceMes != -1 && indiceDepto != -1) {
-            ventas[indiceMes][indiceDepto] = cantidad;
-            System.out.println("Venta insertada con éxito");
+    public static void insertarVenta(String mes, String departamento, int cantidad) {
+        if (validarDatos(mes, departamento)) {
+            int indiceMes = Arrays.asList(meses).indexOf(mes.toLowerCase().replace(" ", ""));
+            int indiceDepto = Arrays.asList(departamentos).indexOf(departamento.toLowerCase().replace(" ", ""));
+            ventas[indiceMes][indiceDepto] += cantidad;
         } else {
-            System.out.println("Mes o departamento inválido");
+            System.out.println("Datos inválidos");
         }
     }
 
     public static String buscarVenta(String mes, String departamento) {
-        mes = mes.toLowerCase().replace(" ", "");
-        departamento = departamento.toLowerCase().replace(" ", "");
-        int indiceMes = Arrays.asList(meses).indexOf(mes);
-        int indiceDepto = Arrays.asList(departamentos).indexOf(departamento);
-
-        if (indiceMes != -1 && indiceDepto != -1) {
-            return "La venta del mes " + mes + " para el departamento " + departamento +
-                   " es: " + ventas[indiceMes][indiceDepto];
+        if (validarDatos(mes, departamento)) {
+            int indiceMes = Arrays.asList(meses).indexOf(mes.toLowerCase().replace(" ", ""));
+            int indiceDepto = Arrays.asList(departamentos).indexOf(departamento.toLowerCase().replace(" ", ""));
+            return "La venta de " + departamento + " en " + mes + " es: " + ventas[indiceMes][indiceDepto];
         } else {
-            return "Mes o departamento inválido";
+            return "Datos inválidos";
         }
     }
 
     public static void eliminarVenta(String mes, String departamento) {
-        mes = mes.toLowerCase().replace(" ", "");
-        departamento = departamento.toLowerCase().replace(" ", "");
-        int indiceMes = Arrays.asList(meses).indexOf(mes);
-        int indiceDepto = Arrays.asList(departamentos).indexOf(departamento);
-
-        if (indiceMes != -1 && indiceDepto != -1) {
+        if (validarDatos(mes, departamento)) {
+            int indiceMes = Arrays.asList(meses).indexOf(mes.toLowerCase().replace(" ", ""));
+            int indiceDepto = Arrays.asList(departamentos).indexOf(departamento.toLowerCase().replace(" ", ""));
             ventas[indiceMes][indiceDepto] = 0;
-            System.out.println("Venta eliminada con éxito");
         } else {
-            System.out.println("Mes o departamento inválido");
+            System.out.println("Datos inválidos");
         }
     }
 
     public static void main(String[] args) {
         insertarVenta("Enero", "Ropa", 1500);
+        insertarVenta("Enero", "Deportes", 2000);
+        insertarVenta("Febrero", "Jugueteria", 3000);
+
         System.out.println(buscarVenta("Enero", "Ropa"));
-        insertarVenta("Febrero", "Deportes", 2000);
-        insertarVenta("Febrero", "Ropa", 2000);
-        insertarVenta("Febrero", "Jugueteria", 2000);
-        insertarVenta("Marzo", "Jugueteria", 3000);
-        insertarVenta("Abril", "Ropa", 2500);
+
+        // Mostrar toda la matriz de ventas
         System.out.println("\nMatriz de ventas:");
         for (int[] filaVentas : ventas) {
             System.out.println(Arrays.toString(filaVentas));
